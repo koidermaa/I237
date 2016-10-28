fb4d6c34311db4189b0444e0d874b7e78bd00b69
Kristo Oidermaa <koiderma@klass2-22-l.intra.itcollege.ee>
2016-10-28 13:35:09 +0300

    esimene töö

--- src/led.c
+++ src/led.c
@@ -3,13 +3,13 @@
 #define	BLINK_DELAY_MS	1000
 int	main	(void)
 {
-				/*	set	pin	3	of	PORTA	for	output*/
+				/*	set	pin	5	of	PORTB	for	output*/
 				DDRA	|=	_BV(DDA3);
 				while(1)	{
-								/*	set	pin	3	high	to	turn	led	on	*/
+								/*	set	pin	5	high	to	turn	led	on	*/
 								PORTA	|=	_BV(PORTA3);
 								_delay_ms(BLINK_DELAY_MS);
-								/*	set	pin	3	low	to	turn	led	off	*/
+								/*	set	pin	5	low	to	turn	led	off	*/
 								PORTA	&=	~_BV(PORTA3);
 								_delay_ms(BLINK_DELAY_MS);
 				}
