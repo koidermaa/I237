0f08fdfc9beb0d872f05a07fb22161b0c5cf1e84
Kristo Oidermaa <koiderma@klass2-22-l.intra.itcollege.ee>
2016-10-28 13:58:10 +0300

    loetavam

--- src/main.c
+++ src/main.c
@@ -1,16 +1,16 @@
-#include	<avr/io.h>
-#include	<util/delay.h>
+#include <avr/io.h>
+#include <util/delay.h>
 #define	BLINK_DELAY_MS	1000
-int	main	(void)
+int	main (void)
 {
-				/*	set	pin	3	of	PORTA	for	output*/
-				DDRA	|=	_BV(DDA3);
-				while(1)	{
-								/*	set	pin	3	high	to	turn	led	on	*/
-								PORTA	|=	_BV(PORTA3);
-								_delay_ms(BLINK_DELAY_MS);
-								/*	set	pin	3	low	to	turn	led	off	*/
-								PORTA	&=	~_BV(PORTA3);
-								_delay_ms(BLINK_DELAY_MS);
-				}
+    /* set pin 5 of PORTB for output*/
+    DDRA |= _BV(DDA3);
+    while(1) {
+        /* set pin 5 high to turn led on */
+       PORTA |= _BV(PORTA3);
+       _delay_ms(BLINK_DELAY_MS);
+       /* set pin 5 low to turn led off */
+       PORTA &= ~_BV(PORTA3);
+       _delay_ms(BLINK_DELAY_MS);
+        }
 }
