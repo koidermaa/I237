0557f83ae2e284781f9d484b865f13cc6445f4f3
Kristo Oidermaa <koiderma@labor-09-l.intra.itcollege.ee>
2016-11-28 19:36:45 +0200

    töötav versioon

--- src/main.c
+++ src/main.c
@@ -17,14 +17,14 @@ void main (void)
     /* Init error console in stderr in UART3 and print version libc and gcc info */
     uart3_initialize();
     stderr = &uart3_out;
-    fprintf(stderr, PSTR(VER_FW "\n"),
-            PSTR(GIT_DESCR), PSTR(__DATE__), PSTR(__TIME__));
-    fprintf(stderr, PSTR(VER_LIBC_GCC "\n"),
-            PSTR(__AVR_LIBC_VERSION_STRING__), PSTR(__VERSION__));
+    fprintf_P(stderr, PSTR(VER_FW "\n"),
+            GIT_DESCR, __DATE__, __TIME__);
+    fprintf_P(stderr, PSTR(VER_LIBC_GCC "\n"),
+            __AVR_LIBC_VERSION_STRING__, __VERSION__);
     /* Init CLI in UART0 and print sudent name */
     uart0_initialize();
     stdout = stdin = &uart0_io;
-    fprintf(stdout, PSTR(STUD_NAME "\n"));
+    fprintf_P(stdout, PSTR(STUD_NAME "\n"));
     /* Init LCD and display student name */
     lcd_init();
     lcd_clrscr();
@@ -45,18 +45,20 @@ void main (void)
 
         /* Ask user to input first letter of month name */
         char letter;
-        fprintf(stdout, PSTR(ENTER_MONTH_NAME));
+        fprintf_P(stdout, PSTR(ENTER_MONTH_NAME));
         fscanf(stdin, "%c", &letter);
         fprintf(stdout, "%c\n", letter);
 
         /* Try to find month and print to CLI and LCD */
-        lcd_goto(0x40);
+	lcd_clr(0x40, 16);
+        lcd_goto(0x40);	
         for (int i = 0; i < 6; i++) {
-            if (!strncmp(&letter, (PGM_P)pgm_read_word(&(months[i])), 1)) {
-                fprintf(stdout, "%s\n", (PGM_P)pgm_read_word(&(months[i])));
-                lcd_puts_P((PGM_P)pgm_read_word(&(months[i])));
-            }
-            /*apply no match clear LCD row 2???*/
+            if (!strncmp_P(&letter, (PGM_P)pgm_read_word(&months[i]), 1)) {
+                fprintf_P(stdout, (PGM_P)pgm_read_word(&months[i]));
+		fprintf(stdout, "\n");
+                lcd_puts_P((PGM_P)pgm_read_word(&months[i]));
+		lcd_puts_P(PSTR(" "));
+            }          
         }
 
         /* Set pin 3 low to turn LED off */
